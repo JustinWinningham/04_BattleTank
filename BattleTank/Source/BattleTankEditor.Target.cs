d05e93f90be9bf577a318b0961ec7a5e852ebd31
daxter154 <daxter154@gmail.com>
2018-07-07 20:20:58 -0700

    Revert to Old
    Yep. All the way back..

--- BattleTank/Source/BattleTankEditor.Target.cs
+++ BattleTank/Source/BattleTankEditor.Target.cs
@@ -5,9 +5,21 @@ using System.Collections.Generic;
 
 public class BattleTankEditorTarget : TargetRules
 {
-	public BattleTankEditorTarget(TargetInfo Target) : base(Target)
+	public BattleTankEditorTarget(TargetInfo Target)
 	{
 		Type = TargetType.Editor;
-        ExtraModuleNames.Add("BattleTank");
+	}
+
+	//
+	// TargetRules interface.
+	//
+
+	public override void SetupBinaries(
+		TargetInfo Target,
+		ref List<UEBuildBinaryConfiguration> OutBuildBinaryConfigurations,
+		ref List<string> OutExtraModuleNames
+		)
+	{
+		OutExtraModuleNames.AddRange( new string[] { "BattleTank" } );
 	}
 }
